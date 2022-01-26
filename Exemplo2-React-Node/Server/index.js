9860edbd9482ba477ef5bd9dc33441ac858356d4
Gabriel Colman <gabrielcolman7@gmail.com>
2022-01-26 14:56:28 -0400

    Axios

--- Exemplo2-React-Node/Server/index.js
+++ Exemplo2-React-Node/Server/index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
-const db =  require("mysql");
+const mysql =  require("mysql");
+const cors = require("cors")
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password:"password",
+    database:"crudgames",
+});
+
+app.use(cors());
+app.use(express.json());
 
 
 app.get('/', (req, res)=> {
-    res.send('hello word')
+    let sql = "INSERT INTO games (name, cost, category) VALUES ('Far Cry 5',  '120', 'Acao')";
+    db.query(sql, (err, result) => {
+        console.log(err);
+    })
 });
+
+app.post("/register", (req, res) =>{
+    const {name} = req.body;
+    const {cost} = req.body;
+    const {category} = req.body;
+
+    let SQL = "INSERT INTO games (name, cost, category) VALUES (?, ?, ?)";
+})
 
     
 app.listen(3001, () => {
